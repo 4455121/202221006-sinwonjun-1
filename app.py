d5772e98bac9c8cc8c6e9aae6504b3b043d9bcf5
4455121 <134983471+4455121@users.noreply.github.com>
2023-06-09 18:37:41 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,50 @@
-Python 3.10.11 (tags/v3.10.11:7d4cc5a, Apr  5 2023, 00:38:17) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+import streamlit as st
 import numpy as np
-... import matplotlib.pyplot as plt
-... import control
-... 
-... num = [100]
-... den = [1, 5, 6]
-... G = control.TransferFunction(num, den)
-... 
-... Gc = G * 1
-... 
-... t = np.linspace(0, 10, 1000)
-... 
-... u = np.ones_like(t)
-... 
-... t, y = control.step_response(Gc, T=t, input=u)
-... 
-... plt.plot(t, y)
-... plt.xlabel('Time')
-... plt.ylabel('Output')
-... plt.title('Step Response')
-... plt.grid(True)
-... plt.show()
-... 
-... omega, mag, phase = control.bode(Gc)
-... 
-... plt.figure()
-... plt.semilogx(omega, mag)
-... plt.xlabel('Frequency (rad/s)')
-... plt.ylabel('Magnitude (dB)')
-... plt.title('Frequency Response')
-... plt.grid(True)
-... 
-... plt.figure()
-... plt.semilogx(omega, phase)
-... plt.xlabel('Frequency (rad/s)')
-... plt.ylabel('Phase (degrees)')
-... plt.title('Phase Response')
-... plt.grid(True)
-... 
-... plt.show()
+import matplotlib.pyplot as plt
+import control
+
+def main():
+    st.title('전달함수 분석')
+
+    # 전달함수 정의
+    num = [100]
+    den = [1, 5, 6]  # (s+2)(s+3) = s^2 + 5s + 6
+    G = control.TransferFunction(num, den)
+
+    # 폐루프 전달함수 계산
+    Gc = G * 1
+
+    # 시간 범위 설정
+    t = np.linspace(0, 10, 1000)
+
+    # 단위 계단 입력 생성
+    u = np.ones_like(t)
+
+    # 시스템 응답 계산
+    t, y = control.step_response(Gc, T=t, input=u)
+
+    # 응답 곡선 그리기
+    fig1, ax1 = plt.subplots()
+    ax1.plot(t, y)
+    ax1.set(xlabel='Time', ylabel='Output', title='Step Response')
+    ax1.grid(True)
+
+    # 주파수 응답 계산
+    omega, mag, phase = control.bode(Gc)
+
+    # 주파수 응답 그래프 그리기
+    fig2, (ax2, ax3) = plt.subplots(2, 1)
+    ax2.semilogx(omega, mag)
+    ax2.set(xlabel='Frequency (rad/s)', ylabel='Magnitude (dB)', title='Magnitude Response')
+    ax2.grid(True)
+
+    ax3.semilogx(omega, phase)
+    ax3.set(xlabel='Frequency (rad/s)', ylabel='Phase (degrees)', title='Phase Response')
+    ax3.grid(True)
+
+    # 그래프를 스트림릿에 표시
+    st.pyplot(fig1)
+    st.pyplot(fig2)
+
+if __name__ == '__main__':
+    main()
