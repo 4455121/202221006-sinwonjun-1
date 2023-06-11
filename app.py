fb8ad390f1c395d6e2d18b8cbeb8992535ff0f4b
4455121 <134983471+4455121@users.noreply.github.com>
2023-06-11 11:54:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def main():
     u = np.ones(t.shape)
 
     # 시스템 응답 계산
-    t, y = control.step_response(Gc, T=t, X0=0, input=u)
+    t, y = control.step_response(Gc, T=t, X0=0, input_='Step')
 
     # 응답 곡선 그리기
     fig1, ax1 = plt.subplots()
